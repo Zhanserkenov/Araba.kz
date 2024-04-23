22381c5b543ecd28a6a603c5b4bcf8c27ec66d05
Zhanserkenov Arsen <73212575+Zhanserkenov@users.noreply.github.com>
2024-04-23 11:54:29 +0500

    TroubleMaker

--- ArabaBackend/arababack/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from arababack.models import Car, Charging, Article
-
-# Register your models here.
-admin.site.register(Car)
-admin.site.register(Charging)
-admin.site.register(Article)
